bd73c0a8b3cea5f8987168bb78811cbcf14a634a
jack <jackjm22@outlook.com>
2019-01-03 17:03:02 -0800

    Add viewflipper and set it up.

--- app/src/main/java/com/tacocats/teknik/MainActivity.kt
+++ app/src/main/java/com/tacocats/teknik/MainActivity.kt
@@ -3,26 +3,32 @@ package com.tacocats.teknik
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ViewFlipper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        val viewFlipper : ViewFlipper = main_view_flipper
+
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                viewFlipper.displayedChild = 0
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_paste -> {
-                message.setText(R.string.title_service_paste)
+                viewFlipper.displayedChild = 1
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_upload -> {
-                message.setText(R.string.title_service_upload)
+                viewFlipper.displayedChild = 2
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_url_shortening -> {
-                message.setText(R.string.title_service_url_shortening)
+                viewFlipper.displayedChild = 3
                 return@OnNavigationItemSelectedListener true
             }
         }
